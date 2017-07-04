057441b3ec8a9dedaf7a3a4721304b5d66d460f1
Sergey Nakhabin <sergey.nachabin@gmail.com>
2017-07-05 01:08:27 +0300

    add theads

--- software/communication.py
+++ software/communication.py
@@ -1,14 +1,25 @@
 import serial, sys, io
-def connect(ser):
+import threading
+def connect(ser,name):
     ser.baudrate = 19200
-    ser.port = 'COM4'
+    ser.port = name
+    ser.timeout=1
+def transmitt(name):
+    ser = serial.Serial()        
+    connect(ser,name)
+    s=''
+    while (s!='9'):
+        
+        ser.open()
+        s=input('>>')
+        ser.write(s.encode('utf-8'))
+        st=ser.read().decode('utf-8')
+        print(st)
+        ser.close()
 
-ser = serial.Serial()        
-connect(ser)    
-s=''
-while (s!='9'):
-    ser.open()
-    s=input('>>')
-    ser.write(s.encode('utf-8'))
-    sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
-    ser.close()
+
+t = threading.Thread(target=transmitt, args=('COM4',))
+t.daemon = True
+t.start()
+
+    
