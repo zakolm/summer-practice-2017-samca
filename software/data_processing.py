a4a4704347969325453c875afe4c19b776fb296e
Evteev Artem <prime.manticore@mail.ru>
2017-07-04 13:38:30 +0300

    restructuring of wave algorithm

--- software/data_processing.py
+++ software/data_processing.py
@@ -1,121 +1,97 @@
-#~~~~~~~~~~~~~~~~~~~~~~
-# ВОЛНОВОЙ АЛГОРИТМ
-#~~~~~~~~~~~~~~~~~~~~~~
 
-import time
-
-def algorithm_lee(field, row, column):
-    global n; n = row 
-    global m; m = column
-    global MAP; MAP = field # копирование исходной матрицы
-
-    # Начальный уровень волны
-    d = 2
-    start = d
-    
-    # Перерисуем карту 
-    for i in range(n):
-        for j in range(m):
+#--------------------------------------------------------------------------
+#---------------------------------Волновой алгоритм------------------------
+#--------------------------------------------------------------------------
+def wave_algorithm(field, nr, nc):
+    # начальный уровень волны
+    wave = 2
+    start = wave
+    # перерисуем карту 
+    for i in range(nr):
+        for j in range(nc):
             if field[i][j] == 1:
-                MAP[i][j] = '#'
+                field[i][j] = '#'
             if field[i][j] == 3:
-                y = i # Координаты
+                y = i # координаты
                 x = j # выхода
-                MAP[i][j] = 0
+                field[i][j] = 0
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     # ~~~~~~~~~~~~~~~~Распространение волны~~~~~~~~~~
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    t1 = time.clock()# запуск секундомера
-    #display(MAP)
+    
     stop = False # предполагаем, что существует непомеченная клетка
-    while MAP[y][x] == 0 and not stop:
+    while field[y][x] == 0 and not stop:
         stop = True # предполагаем, что все свободные клетки уже помечены
-        for i in range(n):
-            for j in range(m):
-                if MAP[i][j] == d: # клетка (i,j) помечена числом d
+        for i in range(nr):
+            for j in range(nc):
+                if field[i][j] == wave: # клетка (i,j) помечена числом wave
                     # проходим по всем непомеченным клеткам-соседям
-                    if free(i, j+1):
-                        MAP[i][j+1] = d + 1
+                    if free(field, nr, nc, i, j+1):
+                        field[i][j+1] = wave + 1
                         stop = False
                     
-                    if free(i-1, j):
-                        MAP[i-1][j] = d + 1
+                    if free(field, nr, nc, i-1, j):
+                        field[i-1][j] = wave + 1
                         stop = False
                         
-                    if free(i, j-1):
-                        MAP[i][j-1] = d + 1
+                    if free(field, nr, nc, i, j-1):
+                        field[i][j-1] = wave + 1
                         stop = False
 
-                    if free(i+1, j):
-                        MAP[i+1][j] = d + 1
+                    if free(field, nr, nc, i+1, j):
+                        field[i+1][j] = wave + 1
                         stop = False
-        d += 1
+        wave += 1
         
-    t2 = time.clock() - t1 # остановка секундомера
+    # ~~~~~~~~~~~~~~~~Восстановление волны~~~~~~~~~~
     
-    #display(MAP)
-    #print('Время распространения волны = {:5.6f} секунд '. format(t2))
-
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # ~~~~~~~~~~~~~~~~Восстановление волны~~~~~~~~~~~
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    # Строим маршрут
-    if MAP[y][x] == 0:
+    if field[y][x] == 0:
         exist = False
-        WAY = None
+        track = None
     else:
         exist = True
-        WAY = [['-'] * m for i in range(n)]
-        WAY[y][x] = '@'
-
-        t = MAP[y][x] # уровень волны
-        while t != start:
-            if route(y, x+1, t):
-                t = MAP[y][x+1]
-                WAY[y][x+1] = '@'
+        track = [['-'] * nc for i in range(nr)]
+        track[y][x] = '@'
+        # пока текущий уровень волны не станет равным начальному
+        while wave != start: 
+            if way_back(field, nr, nc, y, x+1, wave):
+                wave = field[y][x+1]
+                track[y][x+1] = '@'
                 x += 1
             
-            if route(y-1, x, t):
-                t = MAP[y-1][x]
-                WAY[y-1][x] = '@'
+            if way_back(field, nr, nc, y-1, x, wave):
+                wave = field[y-1][x]
+                track[y-1][x] = '@'
                 y -= 1
                 
-            if route(y, x-1, t):
-                t = MAP[y][x - 1]
-                WAY[y][x-1] = '@'
+            if way_back(field, nr, nc, y, x-1, wave):
+                wave = field[y][x - 1]
+                track[y][x-1] = '@'
                 x -= 1
 
-            if route(y+1, x, t):
-                t = MAP[y+1][x]
-                WAY[y+1][x] = '@'
+            if way_back(field, nr, nc, y+1, x, wave):
+                wave = field[y+1][x]
+                track[y+1][x] = '@'
                 y += 1
-        #display(WAY)
+                
+    return track, exist
 
-    return WAY, exist
-
-#------------------------------------------------------------------------------
-#-------------------------------------zzzZZZzzz--------------------------------
-#------------------------------------------------------------------------------
-# Определение свободного пути
-def free(i, j):
-    if i >= 0 and i <= n - 1 \
-    and j >= 0 and j <= m - 1 \
-    and MAP[i][j] == 0:
+# определение свободного пути
+def free(field, nr, nc, i, j):
+    if i >= 0 and i <= nr - 1 \
+    and j >= 0 and j <= nc - 1 \
+    and field[i][j] == 0:
         return True
     return False
     
-# Определение движения для обратного маршрута
-def route(i, j, t):
-    if i >= 0 and i <= n - 1 \
-    and j >= 0 and j <= m - 1 \
-    and MAP[i][j] == t - 1:
+# определение обратного движения по волне
+def way_back(field, nr, nc, i, j, wave):
+    if i >= 0 and i <= nr - 1 \
+    and j >= 0 and j <= nc - 1 \
+    and field[i][j] == wave - 1:
         return True
     return False
 
-# Вывод матрицы
+# вывод матрицы
 def display(array):
     print()
     for line in array:
@@ -123,5 +99,6 @@ def display(array):
             print('{:<3}'.format (element), end = ' ')
         print()
     print()
+
 
 
