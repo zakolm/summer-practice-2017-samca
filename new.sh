1be89d45bad6bf6fea0c26086eac2c3f37c1e339
Zakolesnik Maksim <zakolesnik.m@gmail.com>
2017-07-07 08:42:02 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-for i in {1..6}
-do
-	if [ ! -f file_$i.txt ]
-		then echo Файла file_$i.txt не существует; rm file.txt
-		else cat file_$i.txt > file.txt
-	fi
-	python3 main.py
-	diff -s -q -w f_out.txt out$i.txt
+if [ ! -f file_1.txt ]
+	then echo Файла file_1.txt не существует; rm file.txt
+	else cat file_1.txt > file.txt
+fi
+python3 main.py
+diff -s -q -w f_out.txt out1.txt
 done
